3c8717d5b0ac80f2cec264179d965c7081cde060
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-10-09 21:08:37 +0900

    refactor: generator 함수 view 분리

--- src/App.js
+++ src/App.js
@@ -1,72 +1,8 @@
-import "./App.css";
+import React from "react";
+import GeneratorView from "./GeneratorView";
 
-function* generatorFunction() {
-  console.log("안녕하세요?");
-  yield 1;
-  console.log("제네레이터 함수");
-  yield 2;
-  console.log("function*");
-  yield 3;
-  return 4;
-}
-
-function* watchGenerator() {
-  console.log("모니터링 시작");
-  while (true) {
-    const action = yield;
-    if (action.type === "HELLO") {
-      console.log("안녕하세요?");
-    }
-    if (action.type === "BYE") {
-      console.log("안녕히 가세요.");
-    }
-  }
-}
-
-function App() {
-  const generator = generatorFunction();
-  const watch = watchGenerator();
-
-  return (
-    <div className="container">
-      <button
-        type="button"
-        onClick={() => {
-          console.log(generator.next());
-        }}
-        className="button"
-      >
-        generator 함수 실행
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          console.log(watch.next());
-        }}
-        className="button"
-      >
-        watch 제네레이터 모니터링 실행
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          console.log(watch.next({ type: "HELLO" }));
-        }}
-        className="button"
-      >
-        watch 제네레이터 type HELLO
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          console.log(watch.next({ type: "BYE" }));
-        }}
-        className="button"
-      >
-        watch 제네레이터 type BYE
-      </button>
-    </div>
-  );
-}
+const App = () => {
+  return <GeneratorView />;
+};
 
 export default App;
