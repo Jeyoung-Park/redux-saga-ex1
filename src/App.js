067b5167b06a2cda864af2b5f338f122074a3bee
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-10-09 21:02:39 +0900

    feat: generator 함수 예제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function* generatorFunction() {
+  console.log("안녕하세요?");
+  yield 1;
+  console.log("제네레이터 함수");
+  yield 2;
+  console.log("function*");
+  yield 3;
+  return 4;
+}
+
+function* watchGenerator() {
+  console.log("모니터링 시작");
+  while (true) {
+    const action = yield;
+    if (action.type === "HELLO") {
+      console.log("안녕하세요?");
+    }
+    if (action.type === "BYE") {
+      console.log("안녕히 가세요.");
+    }
+  }
+}
 
 function App() {
+  const generator = generatorFunction();
+  const watch = watchGenerator();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button
+        type="button"
+        onClick={() => {
+          console.log(generator.next());
+        }}
+        className="button"
+      >
+        generator 함수 실행
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          console.log(watch.next());
+        }}
+        className="button"
+      >
+        watch 제네레이터 모니터링 실행
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          console.log(watch.next({ type: "HELLO" }));
+        }}
+        className="button"
+      >
+        watch 제네레이터 type HELLO
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          console.log(watch.next({ type: "BYE" }));
+        }}
+        className="button"
+      >
+        watch 제네레이터 type BYE
+      </button>
     </div>
   );
 }
